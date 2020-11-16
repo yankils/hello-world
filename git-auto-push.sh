068395962546d3a23506d957f5838cfed6960add
vova verh <spamv@i.ua>
2020-11-16 02:57:53 +0200

    merge tester changes 3

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -53,7 +53,7 @@ function write_argument()
 function argument_1()
 {
     git checkout tester
-    mv $home_dir/examlpe-index/index-1.jsp $home_dir/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-1.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add index.jsp
     git commit -m "change index-1"
     git push --all
@@ -71,7 +71,7 @@ function argument_1()
 function argument_2()
 {
     git checkout tester
-    mv $home_dir/examlpe-index/index-2.jsp $home_dir/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-2.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add index.jsp
     git commit -m "change index-2"
     git push --all
@@ -89,7 +89,7 @@ function argument_2()
 function argument_3()
 {
     git checkout tester
-    mv $home_dir/examlpe-index/index-3.jsp $home_dir/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-3.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add index.jsp
     git commit -m "change index-3"
     git push --all
@@ -107,7 +107,7 @@ function argument_3()
 function argument_e()
 {
     git checkout tester
-    mv $home_dir/examlpe-index/index-error.jsp $home_dir/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-error.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add index.jsp
     git commit -m "change index-e"
     git push --all
