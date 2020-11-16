21b9ef22795f5dd53843bfdde2472e25636cb9e9
vova verh <spamv@i.ua>
2020-11-16 03:24:54 +0200

    merge tester changes 1

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -54,7 +54,7 @@ function argument_1()
 {
     git checkout tester
     cp $home_dir/examlpe-index/index-1.jsp $home_dir/webapp/src/main/webapp/index.jsp
-    git add index.jsp
+    git add .
     git commit -m "change index-1"
     git push --all
     git checkout master
@@ -72,7 +72,7 @@ function argument_2()
 {
     git checkout tester
     cp $home_dir/examlpe-index/index-2.jsp $home_dir/webapp/src/main/webapp/index.jsp
-    git add index.jsp
+    git add .
     git commit -m "change index-2"
     git push --all
     git checkout master
@@ -90,7 +90,7 @@ function argument_3()
 {
     git checkout tester
     cp $home_dir/examlpe-index/index-3.jsp $home_dir/webapp/src/main/webapp/index.jsp
-    git add index.jsp
+    git add .
     git commit -m "change index-3"
     git push --all
     git checkout master
@@ -108,7 +108,7 @@ function argument_e()
 {
     git checkout tester
     cp $home_dir/examlpe-index/index-error.jsp $home_dir/webapp/src/main/webapp/index.jsp
-    git add index.jsp
+    git add .
     git commit -m "change index-e"
     git push --all
     git checkout master
