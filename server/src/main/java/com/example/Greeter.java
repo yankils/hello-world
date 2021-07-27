9d992ca0e2f0fef4835d5808e1f4620dd574089a
Monika <trajkovska.monika@yahoo.com>
2021-07-27 15:55:29 +0200

    .

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,9 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-  
+    while (true) {
+    System.out.printf("test");
+}
     return String.format("Hello, %s!", someone);
   
 
--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -27,9 +27,7 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
-   while (true) {
-    System.out.printf("test");
-}
+ 
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
