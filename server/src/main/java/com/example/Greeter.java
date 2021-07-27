c36486971720e1cec9e373756eb7617d755b4ca9
Monika <trajkovska.monika@yahoo.com>
2021-07-27 14:51:05 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,9 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-   while (true ){
-     System.out.printf("Hello");
-   }
+  
     return String.format("Hello, %s!", someone);
   
 
