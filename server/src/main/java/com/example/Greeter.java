c6c152f2efed444f864bcfa79beefba4f3ef609e
Monika <trajkovska.monika@yahoo.com>
2021-07-27 13:21:02 +0200

    .

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   public String greet(String someone) {
    
     return String.format("Hello, %s!", someone);
-    return String.format("Hello, %s!", someone);
+  
 
   } 
 }
--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -27,7 +27,7 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
-
+   String someone = "World";
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
