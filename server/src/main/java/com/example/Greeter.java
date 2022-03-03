7db93a3101eec99e1c9be4defffcdb83af5ac152
ikaudanaveen <98408463+ikaudanaveen@users.noreply.github.com>
2022-03-03 06:32:13 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,7 +6,7 @@ package com.example;
 public class Greeter {
 
   /**
-   * This is a constructor classsssssggs .sssasda
+   * This is a constructor classsssssggs .sssasdasds
    */
   public Greeter() {
 
